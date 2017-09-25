a84d104a828e82cf2414c49766509486da79735b
bladefidz <gamerhacking@gmail.com>
2017-09-25 08:01:03 +0700

    Remove unused codes

--- io/getline.c
+++
@@ -1,8 +0,0 @@
-/* getline: read a line, return length */
-int getline(char *line, int max)
-{
-    if (fgets(line, max, stdin) == NULL)
-        return 0;
-    else
-        return strlen(line);
-}
